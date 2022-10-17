748ce1d80822c4279ffbf9c21d25a49a91add970
Emanukim <emmanue.kimweli@student.moringaschool.com>
2022-10-17 15:20:12 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+
+def greet(name) 
+  puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
+end
+
+
+def add(num1, num2) 
+  return num1 + num2;
+end
+
+
+def halve(n) 
+  if n.class != Integer
+    return nil
+  else
+  return n/2
+
+  end
+# n.class != "interger"? nil : n/2
+  end
